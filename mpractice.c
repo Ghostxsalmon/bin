0d07441ef48049bccfd2f38899bb639d6909e5ca
wyatt <chewebone@gmail.com>
2026-07-03 22:05:30 -0500

    Initial commit: practice C files

---
+++ mpractice.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int is_cold(int temp) {
+    if (temp > 70)
+    return 0;       
+
+    else
+    return 0;
+}
+
+
+int is_adult(int age) {
+    if (age >= 18)
+    return 1;
+
+    else
+    return 0;
+}
